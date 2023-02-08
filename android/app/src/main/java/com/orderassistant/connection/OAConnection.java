5cf4ba20d57f46b0b722219593ab5d914194b9da
leonardopiras <72971585+leonardopiras@users.noreply.github.com>
2023-02-08 18:59:00 +0100

    removed getUsername from OAConnection

--- android/app/src/main/java/com/orderassistant/connection/OAConnection.java
+++ android/app/src/main/java/com/orderassistant/connection/OAConnection.java
@@ -4,7 +4,6 @@ import com.orderassistant.models.*;
 
 public interface OAConnection {
 
-    public String getUsername();
     public WorkService getWorkService();
     public ItemTypeConfiguration getItemTypeConfiguration();
 
